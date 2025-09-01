ed443077e18727d59c112a828685aa9020349685
Vadym <kirilkovadim193@gmail.com>
2025-09-01 17:38:46 +0300

    solution

---
+++ movieapi/cinema/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(null=True, blank=True, max_length=500)
+    duration = models.IntegerField(help_text="Duration in minutes")
+
+    def __str__(self):
+        return self.title
