0ab047d6926dd44e8536e58542317660b59b4899
Vadym <kirilkovadim193@gmail.com>
2025-09-02 10:41:23 +0300

    refactored Movie model to PositiveIntegerField

--- movieapi/cinema/models.py
+++ movieapi/cinema/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=500, default="")
-    duration = models.IntegerField(help_text="Duration in minutes")
+    duration = models.PositiveIntegerField(help_text="Duration in minutes")
 
     def __str__(self):
         return self.title
